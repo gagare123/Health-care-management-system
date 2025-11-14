86387134a44134eec6c09417444ad016e4f9e172
gagare123git config --global user.email lukmanjbadam@gmail.com <lukmanjbadam@gmail.com>
2025-11-14 01:07:32 +0100

    Remove unused Link import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import PatientForm from '@/components/forms/PatientForm';
-import Link from "next/link";
 import PasskeyModal from "@/components/PasskeyModal";
 
 interface SearchParamProps {
