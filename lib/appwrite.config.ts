956ed174757b02ae93b7b2f382185bff9138a4c1
gagare123git config --global user.email lukmanjbadam@gmail.com <lukmanjbadam@gmail.com>
2025-11-16 16:54:57 +0100

    modified Navbar and appwrite.config.ts

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,16 +1,5 @@
 import * as sdk from "node-appwrite";
 
-const client = new sdk.Client();
-
-client
-  .setEndpoint(process.env.ENDPOINT!) // https://cloud.appwrite.io/v1
-  .setProject(process.env.PROJECT_ID!) // Project ID
-  .setKey(process.env.API_KEY!); // API Key with DB + Storage access
-
-export const databases = new sdk.Databases(client);
-export const users = new sdk.Users(client);
-export const storage = new sdk.Storage(client);
-export const messaging = new sdk.Messaging(client);
 
 export const {
   DATABASE_ID,
@@ -22,6 +11,19 @@ export const {
   BUCKET_ID,
   API_KEY,
 } = process.env;
+
+
+const client = new sdk.Client();
+
+client
+  .setEndpoint(process.env.ENDPOINT!) // https://cloud.appwrite.io/v1
+  .setProject(process.env.PROJECT_ID!) // Project ID
+  .setKey(process.env.API_KEY!); // API Key with DB + Storage access
+
+export const databases = new sdk.Databases(client);
+export const users = new sdk.Users(client);
+export const storage = new sdk.Storage(client);
+export const messaging = new sdk.Messaging(client);
 
 // 👇 Optional but useful during debugging
 console.log("🌍 Appwrite Config Check:", {
